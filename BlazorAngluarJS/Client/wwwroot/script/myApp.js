5febd564ecc79c71fafcf4259a30fdd7701e1c3e
Ben Sykes <ben@sykes-family.net>
2021-03-17 18:52:09 -0400

    Trying more stuff, not working

---
+++ BlazorAngluarJS/Client/wwwroot/script/myApp.js
@@ -0,0 +1,17 @@
+﻿
+function reinitAngular() {
+    console.log('reinitAngular called');
+    var injector = $('[ng-app]').injector();
+    var $compile = injector.get('$compile');
+    var $rootScope = injector.get('$rootScope');
+    $compile(this.$el)($rootScope);
+    $rootScope.$digest();
+}
+
+
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
